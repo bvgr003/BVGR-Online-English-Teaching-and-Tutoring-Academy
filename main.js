2f52e5205afb6db43785da18f12cd6b141fd40f9
bvgr003 <148019599+bvgr003@users.noreply.github.com>
2024-11-14 18:45:17 -0600

    Update

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // script.js
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.getElementById("menu-toggle");
     const curtainMenu = document.getElementById("curtain-menu");
 
-    menuToggle.addEventListener("click", function() {
+    menuToggle.addEventListener("click", function () {
         curtainMenu.classList.toggle("active");
     });
 });
